40b1290d5efb42df85f457e2ea9afc6696b08cf4
BlueBug <iopzxz@gmail.com>
2022-01-04 02:22:55 +0900

    1
    1

---
+++ Assets/Scripts/Game/Piece.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace StoryGenerator.Terrain
+{
+	/// <summary>
+	/// Piece of terrain, contains information about the particular terrain piece 
+	/// </summary>
+	public class Piece 
+	{
+		public enum Type {Dirt, Water }
+
+	}
+}
--- Assets/Scripts/TerrainMeshGenerator.cs
+++ Assets/Scripts/TerrainMeshGenerator.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainMeshGenerator : MonoBehaviour
@@ -9,8 +7,8 @@ public class TerrainMeshGenerator : MonoBehaviour
 	Vector3[] verticies;
 	int[] triangles;
 
-	public int xSize = 20;
-	public int zSize = 20;
+	int xSize = 10;
+	int zSize = 10;
 
 	// Start is called before the first frame update
 	void Start()
@@ -22,16 +20,47 @@ public class TerrainMeshGenerator : MonoBehaviour
 
 	}
 
-	private void CreateShape()
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void CreateShape()
 	{
 		verticies = new Vector3[(xSize + 1) * (zSize + 1)];
+		triangles = new int[(xSize) * (zSize) * 6];
 
-		for(int i=0, z = 0;  z <= zSize; z++)
+		for (
+			int i=0, 
+			z = 0;  z <= zSize; z++)
 		{
 			for(int x = 0; x <= xSize; x++)
 			{
-				verticies[i] = new Vector3(x, 0, z);
+				verticies[i] = new Vector3(-.5f+x, 0, -.5f+z);
 				i++;
+			}
+		}
+
+		int triangleIndex = 0;
+		int horizontalVertexRawCount = xSize + 1;
+		for (int j = 0; j < zSize ; j++)
+		{
+			int zIndex = horizontalVertexRawCount * j;
+			for (int i = 0; i < xSize; i++)
+			{
+				Debug.Log(triangleIndex + " " + xSize);
+				int cornerA = zIndex + i;
+				int cornerB = zIndex + i + 1;
+				triangles[triangleIndex + 0] = cornerA;
+				triangles[triangleIndex + 1] = cornerA + horizontalVertexRawCount;
+				triangles[triangleIndex + 2] = cornerA + 1;
+
+				triangles[triangleIndex + 3] = cornerB ;
+				triangles[triangleIndex + 4] = cornerA + horizontalVertexRawCount;
+				triangles[triangleIndex + 5] = cornerB + horizontalVertexRawCount;
+
+				triangleIndex += 6;
 			}
 		}
 
@@ -44,6 +73,7 @@ public class TerrainMeshGenerator : MonoBehaviour
 		mesh.triangles = triangles;
 		mesh.RecalculateNormals();
 	}
+
 	private void OnDrawGizmos()
 	{
 		if( verticies == null)
@@ -51,15 +81,21 @@ public class TerrainMeshGenerator : MonoBehaviour
 			return;
 		}
 
-		for(int i = 0; i < verticies.Length; i++)
+		Gizmos.color = Color.red;
+		for (int x = 0; x < xSize; x++)
+		{
+			for(int z = 0; z < zSize; z++)
+			{
+				Gizmos.DrawSphere(new Vector3(x,0,z), .1f);
+
+			}
+		}
+		Gizmos.color = Color.white;
+		for (int i = 0; i < verticies.Length; i++)
 		{
 			Gizmos.DrawSphere(verticies[i], .1f);
 		}
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
+	
 }
