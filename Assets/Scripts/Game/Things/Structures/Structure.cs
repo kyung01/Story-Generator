e417874b4c88edd750ddfc44a7e49233056e4792
BlueBug <iopzxz@gmail.com>
2022-09-04 01:41:28 +0900

    About to start refraction

--- Assets/Scripts/Game/Things/Structures/Structure.cs
+++ Assets/Scripts/Game/Things/Structures/Structure.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Text;
 using System.Threading.Tasks;
+
+
+public class CollisionModel {
+	Vector2 pos;
+	float weight;
+}
 
 public class Structure : Thing
 {
@@ -13,6 +20,8 @@ public class Structure : Thing
 	int height = 1;
 	public int Width { get { return this.width; } }
 	public int Height { get { return this.height; } }
+
+
 	public Structure():base(TYPE.STRUCTURE)
 	{
 		this.T = TYPE.STRUCTURE;
